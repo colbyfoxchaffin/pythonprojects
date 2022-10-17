c48dee329bb8cf2ad91a9fa2161bc7c520da8ddf
colbyfoxchaffin <colby.chaffin@gmail.com>
2022-10-17 19:15:03 -0400

    Create ex1.py

---
+++ Python_Crash_Course/ex1.py
@@ -0,0 +1,32 @@
+#--------Basic Data Types--------
+
+#Numbers
+
+print(1)       # int
+print(1.0)     # float
+print(1+1)     #Addition
+print(1 * 3)   #Multi
+print(1/2)     #Division
+print(2 ** 4)  #Exponents
+print(4 % 2)   #Modulus
+
+
+#Variable Declaration
+#Use '_' for multi word vars
+var = 2
+print(var)
+
+#Strings
+#Can use double or single quotes. Ex: "Hello", 'Hello'
+
+#Printing Strings
+x = 'hello'
+print(x)
+
+num = 12
+name = 'Sam'
+
+#Print Modifications
+print('My number is {} and my name is {}'.format(num, name))
+
+print('My number is {one} and my name is {two}'.format(one = num, two = name))
