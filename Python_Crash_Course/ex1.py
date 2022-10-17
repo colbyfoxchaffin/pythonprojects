067a52b0852cb99a99e48d21a735e23ef47b0bb6
colbyfoxchaffin <colby.chaffin@gmail.com>
2022-10-17 19:25:18 -0400

    Update ex1.py

--- Python_Crash_Course/ex1.py
+++ Python_Crash_Course/ex1.py
@@ -30,3 +30,41 @@ name = 'Sam'
 print('My number is {} and my name is {}'.format(num, name))
 
 print('My number is {one} and my name is {two}'.format(one = num, two = name))
+
+#Indexing Strings
+s = 'abcdefghijk'
+print(s[0])
+
+#grab 'a' and beyond - Slice Syntax
+print(s[0:])
+
+#grab 'abc' (up to but not including index 3)
+print(s[:3])
+
+#grab 'def'
+print(s[3:6])
+
+
+# Lists
+# A sequence of elements in '[]' separated by ','
+
+my_list = ['a', 'b','c']
+print(my_list)
+#appending
+my_list.append('d')
+print(my_list)
+
+print(my_list[1:3])
+
+my_list[0] = 'NEW'
+
+print(my_list)
+
+#Can nest lists -> list in lists
+
+nest = [1,2,[3,4]]
+print(nest[2][1])
+
+nest = [1,2,3,[4,5,['target']]]
+
+print(nest[3][2][0])
